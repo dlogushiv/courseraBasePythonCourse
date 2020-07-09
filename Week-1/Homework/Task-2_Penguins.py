6aabdf3bc10b1d5b85b550fddd7b520950a07cc9
Name idlogush <ivan.dlogush-ext@jci.com>
2020-07-09 14:45:57 +0300

    second task

---
+++ Week-1/Homework/Task-2_Penguins.py
@@ -0,0 +1,7 @@
+pQuant = input()
+
+print('   _~_    ' * 3, sep=' ')
+print('  (o o)   ' * 3, sep=' ')
+print(' /  V  \\  ' * 3, sep=' ')
+print('/(  _  )\\ ' * 3, sep=' ')
+print('  ^^ ^^   ' * 3, sep=' ')
