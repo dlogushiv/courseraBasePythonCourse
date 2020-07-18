c74075c8ba273d7710328ffe9808cb898b99e315
dlogushiv <dlogushiv@gmail.com>
2020-07-18 21:39:16 +0300

    15 task

---
+++ Week-2/Task-15_If'sCastle.py
@@ -0,0 +1,9 @@
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+e = int(input())
+if (d >= a or d >= b or d >= c) and (e >= a or e >= b or e >= c):
+    print('YES')
+else:
+    print('NO')
